606ed6f2ece1f6cd074c1dd6080e9fe4ae78ab5b
alfred <alfred@okcupid.com>
2005-11-07 17:50:03 +0000

    cleanup

--- rcfiles/freebsd54/dsdc_master.sh
+++ rcfiles/freebsd54/dsdc_master.sh
@@ -5,9 +5,9 @@
 # This is a standard rc.d script to control dsdc.  It responds to all
 # standard rc.d commands:
 #
-#   Usage: dsdcmaster.sh [fast|force](start|stop|restart|rcvar|status|poll)
+#   Usage: dsdc_master.sh [fast|force](start|stop|restart|rcvar|status|poll)
 #
-# In order for it to work, dsdcmaster_enable="YES" needs to be set in 
+# In order for it to work, dsdc_master_enable="YES" needs to be set in 
 # /etc/rc.conf.
 #
 # Author:  Alfred Perlstein <alfred@okcupid.com>
@@ -22,8 +22,6 @@ rcvar=`set_rcvar`
 command="/disk/dsdc/0.2/shared/bin/dsdc"
 dsdc_master_flags="-M -q -d 0xffff"
 pidfile="/var/run/dsdc_master.pid"
-#required_files="/etc/sfs/okws_config"
-#extra_command="reload"
 
 load_rc_config $name
 run_rc_command "$1"
