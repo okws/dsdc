acfb6ecf47748760f899f4ad91a92579ef304331
alfred <alfred@okcupid.com>
2005-11-07 17:50:13 +0000

    add slave rcfile.

---
+++ rcfiles/freebsd54/dsdc_slave.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# DSDC rc.d control script
+#
+# This is a standard rc.d script to control dsdc.  It responds to all
+# standard rc.d commands:
+#
+#   Usage: dsdc_slave [fast|force](start|stop|restart|rcvar|status|poll)
+#
+# In order for it to work, dsdc_slave_enable="YES" needs to be set in 
+# /etc/rc.conf.
+#
+# Author:  Alfred Perlstein <alfred@okcupid.com>
+#
+# $Id$
+#
+
+. /etc/rc.subr
+
+name="dsdc_slave"
+rcvar=`set_rcvar`
+command="/disk/dsdc/0.2/shared/bin/dsdc"
+dsdc_masters="stage0.okcupid.com stage1.okcupid.com"
+dsdc_slave_flags="-s 1024M -d 0xffff"
+dsdc_slave_flags="-S -q ${dsdc_slave_flags} ${dsdc_masters}"
+pidfile="/var/run/dsdc_slave.pid"
+
+load_rc_config $name
+run_rc_command "$1"
+
