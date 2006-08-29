529dcc6fe92c6e0f43e5f1ba39adafc328c82fd5
steve <steve@okcupid.com>
2006-08-29 14:54:33 +0000

    Added support for spawing more then one dsdc_slave instance by adding the variable dsdc_slave_instances to rc.conf

--- rcfiles/freebsd54/dsdc_slave.sh
+++ rcfiles/freebsd54/dsdc_slave.sh
@@ -15,15 +15,18 @@
 # $Id$
 #
 
+
 . /etc/rc.subr
 
 name="dsdc_slave"
 rcvar=`set_rcvar`
+
 load_rc_config $name
 
 if [ "x${dsdc_buildtag}" = "x" ] ; then
 	dsdc_buildtag=shared
 fi
+
 command="/disk/dsdc/0.3/${dsdc_buildtag}/bin/dsdc_slave"
 if [ ! -x ${command} ] ; then
 	echo "${name}: unable to exec ${command}"
@@ -31,7 +34,25 @@ fi
 
 dsdc_slave_flags="-S -q ${dsdc_slave_flags} ${dsdc_masters}"
 
-pidfile="/var/run/dsdc_slave.pid"
 dir=/disk/coredumps/${name}
 cd ${dir}
-run_rc_command "$1"
+
+case "$1" in
+	start)
+		if [ "x${dsdc_slave_instances}" = "x" ] ; then
+			${dsdc_slave_instances}=1
+		fi
+		for i in $(jot ${dsdc_slave_instances}) ; do
+			pidfile="/var/run/${name}-${i}.pid"
+			run_rc_command "$1"
+		done
+		;;
+	stop)
+		echo "Stopping ${name}."
+		pkill ${name}
+		;;
+	*)
+		echo "$1 isn't supposed" 
+		;;
+esac
+
